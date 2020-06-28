f7a29e69b908b689dc0bf7a1559db43a9af155de
Guy <guy@gmail.com>
2020-06-28 13:38:03 +0000

    fail test

--- tests/unit_test.py
+++ tests/unit_test.py
@@ -3,7 +3,7 @@ import adding
 
 class TestBasic(unittest.TestCase):
     def test_me(self):
-        self.assertEqual(adding.add(2, 3), 4)
+        self.assertEqual(adding.add(2, 3), 5)
 
 if __name__ == "__main__":
     unittest.main()
